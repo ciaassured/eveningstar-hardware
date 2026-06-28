3943e5afbd0ca176dc84660a9daff9342742446a
BIOS9 <15035908+BIOS9@users.noreply.github.com>
2026-06-28 16:31:23 +1200

    pcb: use nix for drc check script

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "EveningStar project tooling";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      imports = [
+        ./pcb/nix
+      ];
+    };
+}
